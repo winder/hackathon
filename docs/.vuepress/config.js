4f498744e43990ceb433aca725e8ebf382610a38
Jay Ciruolo <jciruolo@gmail.com>
2019-09-10 21:48:31 -0400

    Example commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: "Algorand Hackathon Developer Guide",
+  description: "Just playing around",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Developer Docs", link: "https://developer.algorand.org" },
+      { text: "Algorand Explorer", link: "https://algoexplorer.io" }
+    ],
+    sidebar: ["/"]
+    // sidebar: ["/", ["/getting-started/", "Example Link"]]
+  }
+};
