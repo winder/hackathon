b4584d4e1289d659d692693ab4f72cdf8cc893e0
Will Winder <wwinder.unh@gmail.com>
2021-01-10 13:32:15 -0500

    Minor changes to java sdk Indexer SearchTransactionsNote example.

--- algorandsamples/v2/java-test/src/main/java/com/algorand/javatest/indexer/SearchTransactionsNote.java
+++ algorandsamples/v2/java-test/src/main/java/com/algorand/javatest/indexer/SearchTransactionsNote.java
@@ -1,58 +1,44 @@
-// SearchTransactionsNote.java
-// requires java-algorand-sdk 1.4.0 or higher (see pom.xml)
 package com.algorand.javatest.indexer;
 
+import com.algorand.algosdk.v2.client.common.Client;
 import com.algorand.algosdk.v2.client.common.IndexerClient;
 import com.algorand.algosdk.v2.client.common.Response;
 import com.algorand.algosdk.v2.client.model.TransactionsResponse;
 
-import java.util.Base64;
-import com.algorand.algosdk.v2.client.common.Client;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class SearchTransactionsNote {
-    public Client indexerInstance = null;
-
-    // utility function to connect to a node
-    private Client connectToNetwork() {
+    /**
+     * Initialize connection to indexer.
+     */
+    private static IndexerClient connectToNetwork() {
         final String INDEXER_API_ADDR = "localhost";
         final int INDEXER_API_PORT = 8981;
-        IndexerClient indexerClient = new IndexerClient(INDEXER_API_ADDR, INDEXER_API_PORT);
-        return indexerClient;
+        return new IndexerClient(INDEXER_API_ADDR, INDEXER_API_PORT);
     }
 
+    /**
+     * Indexer search transaction notes example.
+     */
     public static void main(String args[]) throws Exception {
-        SearchTransactionsNote ex = new SearchTransactionsNote();
-        IndexerClient indexerClientInstance = (IndexerClient) ex.connectToNetwork();
-        byte[] notePrefix = "showing prefix".getBytes();
-        Long round = Long.valueOf(11551185);
-
-        Response<TransactionsResponse> resp = indexerClientInstance.searchForTransactions().notePrefix(notePrefix)
-                .minRound(round).execute();
+        IndexerClient indexerClientInstance = connectToNetwork();
+        Response<TransactionsResponse> resp = indexerClientInstance.searchForTransactions()
+                .notePrefix("showing prefix".getBytes())
+                .minRound(11551185L)
+                .execute();
         if (!resp.isSuccessful()) {
             throw new Exception(resp.message());
         }
-        String response = resp.toString();
 
-        JSONObject jsonObj = new JSONObject(response.toString());
-        System.out.println("Transaction Info: " + jsonObj.toString(2)); // pretty print json
-        JSONArray jsonArray = (JSONArray) jsonObj.get("transactions");
-        if (jsonArray.length() > 0) {
-            try {
-                for (Object o : jsonArray) {
-                    JSONObject ca = (JSONObject) o;
-                    String myNote = (String) ca.get("note");
-                    System.out.println("First Note Info: " + myNote.toString());
-                    byte[] decodedBytes = Base64.getDecoder().decode(myNote);
-                    String decodedString = new String(decodedBytes); 
-                    System.out.println("First Note Info String: " + decodedString);
-                    break;                  
-                }
-            } catch (Exception e) {
-                throw (e);
-            }
+        // pretty print json
+        JSONObject jsonObj = new JSONObject(resp.toString());
+        System.out.println("Transaction Info: " + jsonObj.toString(2));
+
+        int i = 0;
+        for (Transaction tx : resp.body().transactions) {
+            i++;
+            System.out.println("Transaction " + i);
+            System.out.println("  Note Info: " + new String(tx.note));
         }
- 
     }
 }
